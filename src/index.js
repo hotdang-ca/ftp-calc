eeb745c246938c1c16786f99593d5c4c8a948383
James Robert Perih <james.perih@brewninja.net>
2018-11-15 11:51:20 -0600

    Fix process not available in other environments.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import { calcPercentages } from './calc-percentages';
 import { processOnConsole } from './process-console';
 
 // Process for console access
-if (process.argv.length > 0) {
-  const args = processOnConsole(process.argv);
+if (typeof process !== 'undefined') {
+  if (process.argv && process.argv.length > 0) {
+    const args = processOnConsole(process.argv);
 
-  if (typeof args !== 'undefined') {
-    console.log(calcZones(args.watts, args.hr));
+    if (typeof args !== 'undefined') {
+      console.log(calcZones(args.watts, args.hr));
 
-    if (args.percentages && args.percentages.length > 0) {
-      console.log(calcPercentages(args.watts, args.percentages));
+      if (args.percentages && args.percentages.length > 0) {
+        console.log(calcPercentages(args.watts, args.percentages));
+      }
     }
   }
 }
